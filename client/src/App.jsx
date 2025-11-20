063d5d44884996e0c08e3ef88a8e1e912084a370
Olivia Newman <ogn226@lehigh.edu>
2025-11-20 11:40:23 -0500

    Further set up Authentication - changed app.js to validate the token and I think it works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,40 @@
 import {useState, useEffect} from 'react'
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
+
+//Using React Router to manage the pages and navigation
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
+import ProtectedRoute from './components/ProtectedRoute'
+
 function App() {
-  const [apiStatus, setAPIStatus] = useState()
+  // const [apiStatus, setAPIStatus] = useState()
 
-  useEffect(() => {
-    fetch('http://localhost:3000/up')
-    .then(res => res.json())
-    .then(result => {
-      console.log(result.status)
-      setAPIStatus(result)
-  })
-  }, [])
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/up')
+  //   .then(res => res.json())
+  //   .then(result => {
+  //     console.log(result.status)
+  //     setAPIStatus(result)
+  // })
+  // }, [])
 
-  
   
   return (
-    <div>
-      <GoogleLogin 
-      onSuccess ={(credentialResponse) => {
-        //will want to put this code on a longin page but is here for now
-        //Go somewhere after loging in 
-        //cedentialResponse holds user info 
-        //json Object with user info 
-        console.log(jwtDecode(credentialResponse.credential))
-      }} 
-      onError={() => console.log("Login failed")}/>
+    <Router>
+      <Routes>
+        <Route path = "/" element = {<Login />} />
 
-      <h1>To get started, begin editing SRC/App.js</h1>
-      {apiStatus ? <h2>Testing app end point: <div style={{color: apiStatus.status === 'up' ? 'green':'red'}}>{apiStatus.status}</div></h2>:null }
-    </div>
+        {/*Protecting this page*/}
+        <Route path = "/dashboard" element = {<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
+      </Routes>
+    </Router>
+
+
+ 
+
+    //   <h1>To get started, begin editing SRC/App.js</h1>
+    //   {apiStatus ? <h2>Testing app end point: <div style={{color: apiStatus.status === 'up' ? 'green':'red'}}>{apiStatus.status}</div></h2>:null }
+    // </div>
   )
    
 }
