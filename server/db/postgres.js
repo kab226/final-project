da59312e5bb977d0c17ff3d25ecef71ada44fe15
kab226 <kab226@lehigh.edu>
2025-11-26 10:40:11 -0500

    working on database

--- server/db/postgres.js
+++ server/db/postgres.js
@@ -1,27 +1,28 @@
-// import pg from "pg"
+import pg from "pg"
 
-// const {Client} = pg
+const {Client} = pg
 
-// const client = new Client({
-//     host: process.env.POSTGRES_HOST,
-//     port: Number(process.env.POSTGRES_PORT),
-//     database: process.env.POSTGRES_DBNAME,
-//     user: process.env.POSTGRES_USER,
-//     password: process.env.POSTGRES_PASSWORD,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// })
+const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    database: process.env.POSTGRES_DBNAME,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    //added this so hopefully we can still use pg
+    connectionString: process.env.SUPABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 
-// client.connect()
+client.connect()
 
+//THis was a different way to connect but I didn't want to get rid of the pg and it would require rewriting all the CRUD
+// import { createClient } from '@supabase/supabase-js'
+// const supabaseUrl = process.env.SUPABASE_URL
+// const supabaseKey = process.env.SUPABASE_ANON_KEY
+// const supabase = createClient(supabaseUrl, supabaseKey)
 
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-supabase.connect()
 
 export const query = async (text, values) => {
     try{
@@ -38,6 +39,7 @@ export const query = async (text, values) => {
         throw error
     }
 }
+
 
 /**How to use
  * query(qs).then(data) => res.json(data.rows) 
