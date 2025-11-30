1d4f711971267c268bc03231e6a459f3d63cda37
Olivia Newman <ogn226@lehigh.edu>
2025-11-30 15:39:17 -0500

    Not finalized - wanted to push what I have so far

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,54 @@
-import {useState, useEffect} from 'react'
-
 //Using React Router to manage the pages and navigation
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Routes, Route, Link, Outlet} from "react-router-dom"
 import Login from "./pages/Login"
 import Dashboard from "./pages/Dashboard"
 import ProtectedRoute from './components/ProtectedRoute'
+import GroceryList from './pages/GroceryList'
 import HouseholdSetup from './pages/HouseholdSetup'
 
-function App() {
-  
+import MealMateLogo from './FullLogo.png'
 
+//page header 
+const NavHeader = () => {
+  return(
+    <header className = "site-header">
+        <img src={MealMateLogo} alt = "MealMate Logo" className = "app-logo" />
+        <nav className = "header-nav">
+          <Link to = "/dashboard" className = "nav-link">Dashboard</Link>
+          <Link to = "/grocery-list" className = "nav-link">Grocery List</Link>
+          <Link to="/household" className = "nav-link">Change Household</Link>
+        </nav>
+    </header>
+  )
+}
+
+//protect the routes that need authentication from being accessed - outlet is child route
+const ProtectedLayout = () => {
+  return(
+    <ProtectedRoute>
+      <NavHeader />
+      <div className="content">
+        <Outlet/>
+      </div>
+    </ProtectedRoute>
+  )
+}
+
+function App() {
   return (
     <Router>
       <Routes>
         <Route path = "/" element = {<Login />} />
 
         {/*Protecting this page*/}
-        <Route element = {<ProtectedRoute/>}>
+        <Route element = {<ProtectedLayout/>}>
             <Route path = "/household" element = {<HouseholdSetup/>}/>
             <Route path = "/dashboard" element = {<Dashboard/>}/>
+            <Route path = "/grocery-list" element = {<GroceryList/>}/>
         </Route>
-        
       </Routes>
     </Router>
-
-
- 
-
-    //   <h1>To get started, begin editing SRC/App.js</h1>
-    //   {apiStatus ? <h2>Testing app end point: <div style={{color: apiStatus.status === 'up' ? 'green':'red'}}>{apiStatus.status}</div></h2>:null }
-    // </div>
   )
    
 }
